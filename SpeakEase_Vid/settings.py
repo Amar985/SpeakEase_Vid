04d872c14a4f19f83254632c58944a4f46ede3e0
Amar Kumar <amarkumarprashar102002@gmail.com>
2024-04-02 19:36:41 +0530

    changes

--- SpeakEase_Vid/settings.py
+++ SpeakEase_Vid/settings.py
@@ -25,11 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('Secret')
-
+pspass=os.environ.get("pspass")
+psh=os.environ.get("psh")
+psp=os.environ.get("psp")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -79,13 +81,22 @@ WSGI_APPLICATION = 'SpeakEase_Vid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': pspass,
+        'HOST': psh,
+        'PORT': psp,
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -122,9 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR / 'static'
-]
+#STATICFILES_DIRS=[
+#    BASE_DIR / 'static'
+#]
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT=os.path.join(BASE_DIR, 'statticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- SpeakEase_Vid/wsgi.py
+++ SpeakEase_Vid/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpeakEase_Vid.settings')
 
 application = get_wsgi_application()
+app=application
