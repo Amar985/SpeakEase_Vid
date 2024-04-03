c975565cb69ea035792eed84b3bbe0546375de7b
Amar Kumar <amarkumarprashar102002@gmail.com>
2024-04-03 15:35:38 +0530

    a

--- SpeakEase_Vid/settings.py
+++ SpeakEase_Vid/settings.py
@@ -31,8 +31,8 @@ psp=os.environ.get("psp")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 
@@ -139,11 +139,11 @@ STATIC_URL = 'static/'
 #STATICFILES_DIRS=[
 #    BASE_DIR / 'static'
 #]
-STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT=os.path.join(BASE_DIR, 'statticfiles_build', 'static')
+#STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
+#STATIC_ROOT=os.path.join(BASE_DIR, 'statticfiles_build', 'static')
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
