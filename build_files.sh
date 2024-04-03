0ea17f8f9c39ad02f4fcbc36f99840ae3aa54ffd
Amar Kumar <amarkumarprashar102002@gmail.com>
2024-04-03 15:44:31 +0530

    q

---
+++ build_files.sh
@@ -0,0 +1,13 @@
+#pip install -r requirements.txt
+#python3.9 manage.py collectstatic --noinput
+
+#python -m venv --copies /opt/venv && . /opt/venv/bin/activate
+
+# Install dependencies
+#pip install -r requirements.txt
+# Collect static files
+#python3.9 manage.py collectstatic --noinput
+
+# build_files.sh
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
