d3336c1f3d2d9e444d47a67996c12f7e1b9ba079
Amar Kumar <amarkumarprashar102002@gmail.com>
2024-04-03 14:59:00 +0530

    a

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 #python -m venv --copies /opt/venv && . /opt/venv/bin/activate
 
 # Install dependencies
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Collect static files
-#python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
