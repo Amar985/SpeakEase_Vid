cd3d7bc909c9e615c9573a3f07b2ac404ce25f40
Amar Kumar <amarkumarprashar102002@gmail.com>
2024-04-03 17:00:31 +0530

    rt

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
 #pip install -r requirements.txt
 #python3.9 manage.py collectstatic --noinput
 
@@ -14,9 +21,3 @@
 
 #!/usr/bin/env bash    
 # exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
