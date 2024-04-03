170bdbaeada81bdcdf7a72cb75b43edf1664d3b3
Amar Kumar <amarkumarprashar102002@gmail.com>
2024-04-03 15:56:15 +0530

    a

--- build.sh
+++ build.sh
@@ -9,5 +9,14 @@
 #python3.9 manage.py collectstatic --noinput
 
 # build_files.sh
+#pip install -r requirements.txt
+#python3.9 manage.py collectstatic
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+python manage.py collectstatic --no-input
+python manage.py migrate
